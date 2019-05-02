8389b534763babc268daafa2074bb27e6600adf8
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-05-02 09:52:19 -0300

    Adicionando projeto React 2

---
+++ _onebitfood-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.scss";
+import { Container, Section } from 'rbx';
+
+const App = () => (
+    <div>
+        <Section>
+            <Container>
+                One Bit Food
+            </Container>
+        </Section>
+    </div>
+)
+
+export default App;
