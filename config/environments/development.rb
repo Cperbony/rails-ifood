a9c178594b2d445a5e53001bdaf83a039ba7a870
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-05-03 15:12:16 -0300

    configurando cors na api

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   config.active_storage.service = :local
 "#{  config.default_url_options = { host: "localhost:3000" }}"
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  Rails.application.routes.default_url_options[:host] = 'localhost:3001'
 
 
   # Do not eager load code on boot.
