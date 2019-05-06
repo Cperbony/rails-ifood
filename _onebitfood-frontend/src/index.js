f037a104369f44334ce69f8a4cbbba1e3552ce92
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-05-06 13:15:22 -0300

    Adicionando Redux Persist

--- _onebitfood-frontend/src/index.js
+++ _onebitfood-frontend/src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import Store from "./store";
 import "./index.css";
+import {PersistGate} from 'redux-persist/integration/react';
+import persistor from './store/persisted_store';
+
 
 ReactDOM.render(
     <Provider store={Store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App/>
+        </PersistGate>
     </Provider>,
-    document.getElementById('root'));
+    document.getElementById('root')
+);
 serviceWorker.unregister();
