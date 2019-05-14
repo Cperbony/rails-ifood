2652aedc65c1f37bae75fd0cbde96c15c4fce330
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-05-14 14:18:48 -0300

    Modal Pedido

--- _onebitfood-frontend/src/reducers/newOrderReducer.js
+++ _onebitfood-frontend/src/reducers/newOrderReducer.js
@@ -1,9 +1,10 @@
-import { ADD_ORDER_ITEM } from "../actions/action_types";
+import { ADD_ORDER_ITEM, REMOVE_ORDER_ITEM } from "../actions/action_types";
 
-export default (state = { restaurant: [], order: [] }, action) => {
-    switch (action.type) {
+export default (state = { order: [] }, action) => {
+    switch(action.type) {
         case ADD_ORDER_ITEM:
             let order = []
+
             if(state.restaurant && state.restaurant.id == action.restaurant.id) {
                 order = state.order;
             }
@@ -20,6 +21,15 @@ export default (state = { restaurant: [], order: [] }, action) => {
                 order: order
             }
 
+        case REMOVE_ORDER_ITEM:
+            const indexToRemove = state.order.indexOf(action.orderItem)
+            const newOrder = [...state.order];
+            newOrder.splice(indexToRemove, 1);
+
+            return {
+                ...state,
+                order: newOrder
+            }
         default:
             return state
     }
